0f8ba4c204e23ff8bf2473e926b420b3ae698be9
danctila <danctila@gmail.com>
2023-12-03 20:12:26 -0500

    Added about component

--- CALCULATORFRONTEND/src/Components/About.tsx
+++ CALCULATORFRONTEND/src/Components/About.tsx
@@ -1,9 +1,70 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Link,
+  ListItem,
+  Text,
+  UnorderedList,
+  VStack,
+} from "@chakra-ui/react";
 
 const About = () => {
   return (
     <>
-      <Text>Hello im about</Text>
+      <Box bg="#E8E8E8" minW="470px">
+        <HStack justify="center" pt="40px" minW="470px" px="15px" pb="250px">
+          <VStack w="470px" spacing="8px" align="left">
+            <Text fontSize="20px" fontWeight="bold" color="black">
+              About
+            </Text>
+            <Text fontSize="16px" fontWeight="normal" color="black">
+              Nhsgrader.com was made purely as a tool for others to use. If you
+              like Nhsgrader.com please consider spreading the word to students
+              around the school.
+            </Text>
+            <Text fontSize="16px" fontWeight="semibold" color="black">
+              Creator
+            </Text>
+            <Text fontSize="16px" fontWeight="normal" color="black">
+              This entire website was coded from scratch by Dylan Anctil using
+              ReactJS as a framework for TypeScript. If you want to know more,
+              check out this{" "}
+              <Link color="blue" href="https://dylanslibrary.com">
+                portfolio website{" "}
+              </Link>
+              for details.
+            </Text>
+            <Text fontSize="16px" fontWeight="semibold" color="black">
+              Feedback
+            </Text>
+            <Text fontSize="16px" fontWeight="normal" color="black">
+              Please help improve this site by sending feedback to the email
+              below.
+              <br />
+              <br />
+              Please send feedback about:
+              <br />
+            </Text>
+            <UnorderedList>
+              <ListItem>Suggestions.</ListItem>
+              <ListItem>Corrections.</ListItem>
+              <ListItem>Missing content.</ListItem>
+            </UnorderedList>
+            <Text fontSize="16px" fontWeight="normal" color="black">
+              <br />
+              Send feedback to:{" "}
+              <Link
+                color="blue"
+                onClick={() =>
+                  (window.location.href = "mailto:danctila@gmail.com")
+                }
+              >
+                Danctilla@gmail.com
+              </Link>
+            </Text>
+          </VStack>
+        </HStack>
+      </Box>
     </>
   );
 };
--- CALCULATORFRONTEND/src/Components/Gradehelp.tsx
+++ CALCULATORFRONTEND/src/Components/Gradehelp.tsx
@@ -6,30 +6,42 @@ const Gradehelp = () => {
     <>
       <HStack justify="center" minW="470px" mt="40px" pb="250px">
         <VStack w="470px" align="left" spacing="1px">
-          <Text fontSize="20px" fontWeight="bold" color="black" pb="10px">
+          <Text
+            fontSize="20px"
+            fontWeight="bold"
+            color="black"
+            pb="10px"
+            px="15px"
+          >
             Help
           </Text>
-          <Text fontSize="16px" fontWeight="semibold" color="black">
+          <Text fontSize="16px" fontWeight="semibold" color="black" px="15px">
             Example
           </Text>
-          <Text fontSize="16px" fontWeight="normal" color="black">
+          <Text fontSize="16px" fontWeight="normal" color="black" px="15px">
             Homework assignment scored 8/10 in the AFL 20% category.
           </Text>
           <Formeg />
-          <Text fontSize="16px" fontWeight="semibold" color="black">
+          <Text fontSize="16px" fontWeight="semibold" color="black" px="15px">
             Note
           </Text>
-          <Text fontSize="16px" fontWeight="normal" color="black">
+          <Text fontSize="16px" fontWeight="normal" color="black" px="15px">
             The assignment field is optional and will not effect calculation.
           </Text>
-          <Text fontSize="16px" fontWeight="normal" color="black">
+          <Text fontSize="16px" fontWeight="normal" color="black" px="15px">
             The default value for the category field is AFL 20% with no option
             selected.
           </Text>
-          <Text fontSize="16px" fontWeight="semibold" color="black" mt="25px">
+          <Text
+            fontSize="16px"
+            fontWeight="semibold"
+            color="black"
+            mt="25px"
+            px="15px"
+          >
             Feedback
           </Text>
-          <Text fontSize="16px" fontWeight="normal" color="black">
+          <Text fontSize="16px" fontWeight="normal" color="black" px="15px">
             If issues persist or you have any questions please email{" "}
             <Link
               color="blue"
